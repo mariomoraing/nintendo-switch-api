f94d764ec14658dc2763119d490285847919ebc5
Mario Mora Núñez <mariomora.ingeniero@gmail.com>
2025-03-28 10:56:56 -0300

    Add Helmet to project

--- index.js
+++ index.js
@@ -1,11 +1,25 @@
 require('dotenv').config();
 const express = require('express');
+const helmet = require('helmet');
+
 const gameRoutes = require('./src/routes/gameRoutes');
 const authRoutes = require('./src/routes/authRoutes');
 
-
 const app = express();
 const port = process.env.PORT || 3000;
+
+app.use(helmet({
+    contentSecurityPolicy: false, 
+    hsts: {
+        maxAge: 31536000, // 1 año en segundos
+        includeSubDomains: true,
+        preload: true
+    },
+    frameguard: {
+        action: 'deny' // Evita que la API sea embebida en iframes
+    }
+}));
+
 
 app.use(express.json());
 app.use('/api', gameRoutes);
